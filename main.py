2de1c1e62fa8de5a7bed638edadab1375e5aae63
AkinaLi-M <125566464+AkinaLi-M@users.noreply.github.com>
2023-02-17 09:01:46 +0000

    -

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ def find_mismatch(text):
             # Process opening bracket, write your code here
             opening_brackets_stack.append(Bracket(next, i + 1))
             
-
         if next in ")]}":
             # Process closing bracket, write your code here
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
